e827e10582cff3f22571fac118d45d6361ec1124
HaybersonCastillo <t85144547@gmail.com>
2023-11-26 00:27:29 -0500

    Se le agrego localStorage al formulario de registro

---
+++ appRegister.js
@@ -0,0 +1,21 @@
+var form = document.getElementById("box_form");
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    var email = document.getElementById("correo").value;
+    var name = document.getElementById("nombre").value;
+    var password = document.getElementById("password").value;
+
+    var formData = {
+        Email: email,
+        Nombre: name,
+        Password: password
+    };
+
+    var formDataJson = JSON.stringify(formData);
+
+    localStorage.setItem("formData", formDataJson);
+
+    console.log(localStorage);
+
+});
