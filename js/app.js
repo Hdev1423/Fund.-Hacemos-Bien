dbcc9db0e075c4935ef1399c2a26fe12250068db
HaybersonCastillo <t85144547@gmail.com>
2023-11-26 21:40:00 -0500

    Se elimino appLogin.js y se creo app.js con el registro de localStorage

---
+++ js/app.js
@@ -0,0 +1,56 @@
+function almacenarUsuario() {
+    // Captura los datos del formulario
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var correo = document.getElementById("correo").value;
+    var contraseña = document.getElementById("password").value;
+    var genero = document.getElementById("genero").value;
+
+    // Crea un objeto con los datos del usuario
+    var data = {
+        nombre: nombre,
+        apellido: apellido,
+        correo: correo,
+        contraseña: contraseña,
+        genero: genero
+    };
+
+    // Convierte el objeto en una cadena JSON
+    var dataJSON = JSON.stringify(data);
+
+    // Guarda los datos del usuario en el localStorage
+    localStorage.setItem('usuario', dataJSON);
+
+    // Guarda el nombre del usuario en localStorage para usarlo en la barra de navegación
+    localStorage.setItem('nombreUsuario', nombre);
+
+    // Redirige al usuario a la página de inicio de sesión
+    window.location.href = "http://127.0.0.1:5500/html/Login.html";
+}
+
+function iniciarSesion() {
+    
+    var correo = document.getElementById("correo").value;
+    var password = document.getElementById("password").value;
+  
+    
+    var usuarioJSON = localStorage.getItem('usuario');
+  
+    if (usuarioJSON) {
+     
+        var usuario = JSON.parse(usuarioJSON);
+  
+        
+        if (correo === usuario.correo && password === usuario.contraseña) {
+            alert("Inicio exitoso")
+        } else {
+            
+            alert('Cuenta no registrada o datos incorrectos.');
+        }
+    } else {
+        
+        alert('Cuenta no registrada o datos incorrectos.');
+    }
+  }
+
+  window.href = "index.html";
